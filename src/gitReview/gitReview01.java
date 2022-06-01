f1936d4b901d01f1a1c5b431474d0be16466b705
nnasraty <nnasraty@gmail.com>
2022-05-31 21:51:49 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -4,7 +4,10 @@ public class gitReview01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("today we learn github");
+		System.out.println("our teacher asshole");
+		System.out.println(" he has mental issue too");
+		
 	}
 
 }
